8ff6c091b162a3773b0d89e3363c9831a200c533
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-28 14:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, values) {
   return Object.assign({}, object, {[key] : values})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object[key] = value
+}
